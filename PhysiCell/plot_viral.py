f95c38c84ecc4b4cda89505ef6d9000b6066a27a
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-10-13 17:38:47 -0400

    Latest version that's almost working

--- PhysiCell/plot_viral.py
+++ PhysiCell/plot_viral.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../../python-loader')
 from pyMCDS import pyMCDS
 import pickle, os
+import numpy as np
 
 # mcds = pyMCDS("output{:08d}.xml".format(0), "output/")
 # import IPython;IPython.embed()
@@ -10,24 +11,35 @@ import pickle, os
 if os.path.exists("data_arr.pickle"):
     with open("data_arr.pickle", "rb") as f:
         dat = pickle.load(f)
-    virion_dat, disc_vir, cell_dat, immune_dat, cont_immune = dat
+    virion_dat, disc_vir, cell_dat, immune_dat, cont_immune, frac_inf, assembled_vir = dat
 else:
     virion_dat = []
     cell_dat = []
     disc_vir = []
     immune_dat = []
     cont_immune = []
+    frac_inf = []
+    assembled_vir = []
     for i in range(0,101):
         mcds = pyMCDS("output{:08d}.xml".format(i), "output/")
-        # import IPython;IPython.embed()
-        # sys.exit() 
-        total_volume = 2494
-        virion_dens = mcds.data['continuum_variables']['virion']['data'].sum()
-        virions = virion_dens*total_volume
+        total_volume = 8000 
+        virion_dens = mcds.data['continuum_variables']['virion']['data']
+        virions = (virion_dens*total_volume).sum()
         virion_dat.append(virions)
 
-        disc_vir_cnt = mcds.data['discrete_cells']['virion'].sum()
+        vir_arr = mcds.data['discrete_cells']['virion']
+        infected = vir_arr > 0
+        frac_inf_tp = len(vir_arr)/len(np.where(infected)[0]) if len(np.where(infected)[0]) else 0
+        frac_inf.append(frac_inf_tp)
+
+        # print("output {}".format(i))
+        # import IPython;IPython.embed()
+
+        disc_vir_cnt = vir_arr.sum()
         disc_vir.append(disc_vir_cnt)
+
+        assembl_vir_cnt = mcds.data['discrete_cells']['assembled_virion'].sum()
+        assembled_vir.append(assembl_vir_cnt)
 
         live_cell_cnt = (mcds.data['discrete_cells']['cell_type']==1).sum()
         cell_dat.append(live_cell_cnt)
@@ -42,19 +54,22 @@ else:
         cont_immune_cnt = cont_immune_cnt * total_volume
         cont_immune.append(cont_immune_cnt)
         
-    dat = (virion_dat, disc_vir, cell_dat, immune_dat, cont_immune)
+    dat = (virion_dat, disc_vir, cell_dat, immune_dat, cont_immune, frac_inf, assembled_vir)
     
     with open("data_arr.pickle", "wb") as f:
         pickle.dump(dat, f)
 
+print(frac_inf)
+
 import matplotlib.pyplot as plt
 import seaborn
 
-plt.plot(virion_dat, label="continuum virions")
-plt.plot(disc_vir, label="discrete virions")
+plt.plot(virion_dat, label="environment virions")
+plt.plot(disc_vir, label="cell virions")
+plt.plot(assembled_vir, label="assembled virions")
 plt.plot(cell_dat, label="live cells")
-plt.plot(immune_dat, label="immune cells")
-plt.plot(cont_immune, label="continuum immune resp")
+# plt.plot(immune_dat, label="immune cells")
+# plt.plot(cont_immune, label="continuum immune resp")
 plt.legend(frameon=False)
 plt.gca().set(yscale="log")
 plt.xlabel("time (hours)")
